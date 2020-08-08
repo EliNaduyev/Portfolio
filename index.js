7848a2485c9987ca03a06cb6c7ae08de36b07049
eli <33352779+EliNaduyev@users.noreply.github.com>
2020-08-08 21:24:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
 window.onload = function(){ 
     let intro = document.querySelector(".intro-animation")
-    intro.classList.add('intro-show')
+    if(intro)
+        intro.classList.add('intro-show')
+
+    
+    let email = document.querySelector(".intro-contact-info")
+    if(email)
+        email.classList.toggle("show-intro-contact-info")
   }
 
 const changeNavColor = () => {
     let sideNavbar = document.querySelector('.top-fixed-nav')
-    let sideNavbarPosition = sideNavbar.getBoundingClientRect().top
+    if(sideNavbar){
+        let sideNavbarPosition = sideNavbar.getBoundingClientRect().top
 
-    if (window.pageYOffset !== sideNavbarPosition)
-        sideNavbar.style.backgroundColor  = 'rgba(0, 0, 0, 0.4)'
-    else
-        sideNavbar.style.backgroundColor  = 'transparent'
+        if (window.pageYOffset !== sideNavbarPosition)
+            sideNavbar.style.backgroundColor  = 'rgba(0, 0, 0, 0.4)'
+        else
+            sideNavbar.style.backgroundColor  = 'transparent'
+    }
 }
 
-
-const showEmail = () => {
-    let email = document.querySelector(".intro-contact-info")
-    email.classList.toggle("show-intro-contact-info")
-}
 
 const closeNav = () =>{
     let sideNavbar = document.querySelector('.side-navbar')
